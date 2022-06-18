aaf2ea4b6d4bbb49c8624df885b93af686c6330d
leah karienye <karienyeleah@gmail.com>
2022-06-18 21:42:37 +0300

    commit tree 4b803735dab53891b69d2e3da91763ee32f8b0cc

---
+++ index.js
@@ -0,0 +1,62 @@
+const myEach = function (collection, callBack){
+   let collections = [1, 2, 3, 4]
+   collections.forEach(collection => {
+       return collections.map;
+   });
+    return collection
+}
+const myMap = function (collection, callBack){
+    
+    collection ([1,2,3,4], function(num){ num * 2});
+
+}
+const myReduce = function (collection, callBack, acc){
+    
+    return callBack;
+
+}
+const myFind = function (collection, predicate){
+    
+    return collection;
+
+}
+const myFilter = function (collection, predicate){
+    
+    return [];
+
+}
+const mySize = function ([collection]){
+    
+    return collection.length;
+
+}
+const myFirst = function (array, [n]){
+    
+    return array || [n];
+
+}
+const myLast = function (array, [n]){
+    
+    return [n] || array;
+
+} 
+const mySortBy = function (array, callBack){
+    
+    return [];
+
+}
+const myFlatten = function (array, [shallow], newArr=[]){
+    
+    return [];
+
+}
+const myKeys = function (object){
+    
+    return [];
+
+}
+const myValues = function (object){
+    
+    return [];
+
+}
